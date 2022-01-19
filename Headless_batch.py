453e0457729fa1489b1a8c5583c072e3f1c8bc58
1 <77960507+Wiz1code@users.noreply.github.com>
2022-01-19 19:14:54 +0800

    Add files via upload

---
+++ Headless_batch.py
@@ -0,0 +1,63 @@
+# @Time    : 2022/1/19
+# @Author  : Wiz1Code
+# @FileName: 无头浏览器导入Hydro
+
+import time
+from selenium.webdriver import Edge
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.select import Select
+
+path = "MicrosoftWebDriver.exe"
+EDGE = {
+            "browserName": "MicrosoftEdge",
+            "version": "",
+            "platform": "WINDOWS",
+            "ms:edgeOptions": {
+                'extensions': [],
+                'args': [
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222',
+                ]}
+        }
+web= Edge(executable_path=path, capabilities=EDGE)
+
+print("请输入您想从SYZOJ中导入的起始PROBLEM ID")
+beg = int(input())
+
+print("请输入您想从SYZOJ中导入的结束PROBLEM ID")
+ed = int(input())
+
+print("请输入要定义的起始编号")
+pos = int(input())
+
+web.get("http://url/problem/import/syzoj)  #此处填写你自己的OJ url 
+
+time.sleep(1)
+
+web.find_element_by_name("uname").send_keys("xxx")  #此处填写用户名
+web.find_element_by_name("password").send_keys("xxxxx")   #此处填写密码
+
+time.sleep(1)
+
+web.find_element_by_xpath("/html/body/div[2]/div[4]/div/div/div/form/div[5]/div/div/input[2]").click()
+
+print("登录成功")
+
+time.sleep(1)
+
+for i in range(beg,ed):
+    url = "https://syzoj.com/problem/"  
+    url += str(i)
+
+    pid = "P" + str(pos)  #默认编号为P开头，可自由更改
+    
+    web.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[1]/div/div/form/div[1]/div[1]/label/input").send_keys(url)
+    web.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[1]/div/div/form/div[1]/div[2]/label/input").send_keys(pid)
+   
+    web.find_element_by_xpath("/html/body/div[2]/div[3]/div/div[1]/div/div/form/div[2]/div/button[1]").click()
+    web.back();
+    print("成功从SYZOJ导入第%s题,定义题目编号为:%s" %(i, pid))
+    pos += 1
+
+web.close()
---
+++ README.md
@@ -0,0 +1,27 @@
+# Hydro_Batch_Import
+本脚本目前只适用于Edge浏览
+如果需要在其他版本上运行，只需更改相应驱动即可
+本处不作过多阐述
+
+从SYZOJ批量导入题目进Hydro搭建的OJ自动化脚本
+
+#### 版本说明
+一共有两个版本
+分别是有头【Header_batch.py】和无头【Headless_batch.py】
+
+有头版本即可以看到浏览器的操作运行，能够看到画面
+无头版本即只可以在控制台看到操作信息，因为中间减少了浏览器渲染等过程，效率相对较高。
+
+此处建议使用无头版本
+
+#### 环境配置
+在使用前，
+请先使用“pip install selenium”命令安装库
+另外需要去官网下载相对应的Edge版本包和配置环境变量
+可以参考[此处](https://blog.csdn.net/tk1023/article/details/109078613?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522164257747016780255212244%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=164257747016780255212244&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_ecpm_v1~rank_v31_ecpm-1-109078613.pc_search_result_cache&utm_term=selenium+edge&spm=1018.2226.3001.4187)
+
+#### 使用说明
+<img src="https://pic.imgdb.cn/item/61e7f14f2ab3f51d913fdff6.jpg">
+<img src="https://pic.imgdb.cn/item/61e7f02d2ab3f51d913edd47.jpg">
+需更改源码中如上图的OJURL,uname和passwd(OJ的SU账号)
+
